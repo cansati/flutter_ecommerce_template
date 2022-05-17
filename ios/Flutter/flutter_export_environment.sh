249bca427176659102af33531706ea4f4dc027f7
username <abudbasha1997@gmail.com>
2022-05-17 14:09:40 +0300

    Commit message

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/cansati/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/cansati/AndroidStudioProjects/flutter_ecommerce_template"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\abudb\StudioProjects\flutter_ecommerce"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=1.0.1"
